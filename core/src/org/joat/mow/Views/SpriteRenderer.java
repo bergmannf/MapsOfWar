96425444e127bb4bee096deba3ec1a66c99a2a0f
Florian Bergmann <Bergmann.F@gmail.com>
2014-07-23 23:29:17 +0200

    Sprites are now cached in the view.

--- core/src/org/joat/mow/Views/SpriteRenderer.java
+++ core/src/org/joat/mow/Views/SpriteRenderer.java
@@ -1,5 +1,6 @@
 package org.joat.mow.Views;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -7,26 +8,43 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import org.joat.mow.Assets;
 import org.joat.mow.Model.AbstractGameObject;
+import sun.misc.Cache;
+
+import java.util.HashMap;
 
 /**
  * Renders a sprite to a given location.
  */
 public class SpriteRenderer {
+    private final String TAG = SpriteRenderer.class.getName();
+    private final HashMap<String, Sprite> spriteCache;
+
+    public SpriteRenderer() {
+        spriteCache = new HashMap<String, Sprite>();
+    }
 
     public void render(SpriteBatch batch, AbstractGameObject o) {
         final Vector2 position = o.position;
         final Vector2 dimension = o.dimension;
-        final Sprite sprite = initGraphics(o);
+        final Sprite sprite = getSprite(o);
         batch.draw(sprite, position.x, position.y, dimension.x, dimension.y);
     }
 
-    protected Sprite initGraphics(AbstractGameObject o) {
+    protected Sprite getSprite(AbstractGameObject o) {
+        Sprite sprite;
+        final String spriteKey = o.getSpriteName();
+        sprite = spriteCache.get(spriteKey);
+        if (sprite != null) {
+            Gdx.app.log(TAG, "Cache hit for sprite " + spriteKey);
+            return sprite;
+        }
         TextureAtlas atlas = Assets.instance.getUnitSpriteAtlas();
         TextureRegion region = atlas.findRegion(o.getSpriteName());
         if (region != null) {
-            Sprite s = new Sprite(region);
-            s.setScale(o.scale.x, o.scale.y);
-            return s;
+            sprite = new Sprite(region);
+            sprite.setScale(o.scale.x, o.scale.y);
+            spriteCache.put(spriteKey, sprite);
+            return sprite;
         } else {
             throw new IllegalArgumentException("sprite does not exist "
                     + o.getSpriteName());
