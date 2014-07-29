0126c66ea513b56a9267720910d08528fa0f09fe
Florian Bergmann <Bergmann.F@gmail.com>
2014-07-29 22:54:16 +0200

    Added concrete tests for cell-membership in collection.

--- core/test/Model/GridTests.java
+++ core/test/Model/GridTests.java
@@ -2,13 +2,13 @@ package Model;
 
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
-import junit.framework.Assert;
 import org.joat.mow.MapsOfWar;
 import org.joat.mow.Model.Cell;
 import org.joat.mow.Model.Grid;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Assert;
+import static org.hamcrest.CoreMatchers.*;
 
 import java.util.List;
 
@@ -29,8 +29,24 @@ public class GridTests {
     @Test
     public void neighborsTest() {
         Grid grid = new Grid(3, 3);
+        Cell[][] cells = grid.getCells();
         Cell cell = new Cell(1, 1);
         List<Cell> n = grid.neighbors(cell);
         Assert.assertEquals(4, n.size());
+        Assert.assertThat(n, hasItem(cells[1][0]));
+        Assert.assertThat(n, hasItem(cells[0][1]));
+        Assert.assertThat(n, hasItem(cells[2][1]));
+        Assert.assertThat(n, hasItem(cells[1][2]));
+
+        cell = new Cell(0, 0);
+        n = grid.neighbors(cell);
+        Assert.assertEquals(2, n.size());
+        Assert.assertThat(n, hasItem(cells[1][0]));
+        Assert.assertThat(n, hasItem(cells[0][1]));
+    }
+
+    @Test
+    public void pathTest() {
+        Grid grid = new Grid(3, 3);
     }
 }
